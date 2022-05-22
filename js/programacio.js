e1f3d6acae3b137af9d15be61c41ee438dc949df
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-05-22 17:28:15 -0600

    Se termina diseño de menu de Windows

--- js/programacio.js
+++ js/programacio.js
@@ -1,1 +1,11 @@
-console.log('Hola')
+// VÍNCULO CON LA ETIQUETA
+var ico_carpeta = document.getElementById('menu');
+var ventana =  document.getElementById('ventana');
+
+//FUNCIONES
+function activar(){
+   ventana.classList.add("activo");
+}
+
+//EVENTO
+ico_carpeta.addEventListener('click', activar);
